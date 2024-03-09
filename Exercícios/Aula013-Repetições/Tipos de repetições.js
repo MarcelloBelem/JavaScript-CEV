6751c6a26da8c771fabd9b8cbf428d73121484f3
MarcelloBelem <devmarcellobel@gmail.com>
2024-03-09 14:06:24 -0300

    Tipos de repetições

---
+++ Exercícios/Aula013-Repetições/Tipos de repetições.js
@@ -0,0 +1,38 @@
+/*
+    Repetição sem usar os comando, Repetição crua
+
+console.log("Teste 1")
+console.log("Teste 2")
+console.log("Teste 3")
+console.log("Teste 4")
+console.log("Teste 5")
+*/
+
+
+/*
+    Repetição de teste logico no inicio, Repetiçao TestaFaz
+
+var c = 1
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/*
+    Repetição de teste logico no final, Repetição FazTesta
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 5)
+*/
+
+/*
+    Repetição com Variavel de Controle, Repetição incrementada
+
+for(var c = 1; c <= 10;c++) {
+    console.log(`Passo ${c}`)
+}
+*/
---
+++ Exercícios/Aula014/Depuração.js
@@ -0,0 +1,7 @@
+console.log("Vai Comerçar...")
+for(c=1;c<=5;c++) {
+    console.log(`Etapa ${c}`)
+}
+console.log("FIM")
+
+//
