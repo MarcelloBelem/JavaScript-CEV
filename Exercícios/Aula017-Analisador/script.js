bda4ef92aa610dcdfb7e80307141e93d62d7bc54
MarcelloBelem <devmarcellobel@gmail.com>
2024-03-16 19:42:13 -0300

    Analisador de Números concluido

--- Exercícios/Aula017-Analisador/script.js
+++ Exercícios/Aula017-Analisador/script.js
@@ -1,39 +1,41 @@
-var valueUser = document.getElementById("valor")
-var array = document.getElementById("array")
-var spaceAnalyze = document.getElementById("spaceanalyzed")
-var valores = []
-
-
+let valueUser = document.getElementById("valor")
+let array = document.getElementById("array")
+let spaceAnalyze = document.getElementById("spaceanalyzed")
+let valores = []
 
 function add() {
-    var valueData = Number(valueUser.value)
+    let valueData = Number(valueUser.value)
     
-
-    if (valueUser.value.length == 0 || valueData > 100 || valueData <= 0) {
-        alert("[ERRO!] Apenas números entre 1 e 100!")
+    if (valueUser.value.length == 0 || valueData <= 0 || valueData > 100) {
+        alert("[ERRO!] Apenas valores entre 1 e 100!")
     } else {
         valores.push(valueData)
-        array.innerHTML += `<p>O valor ${valueData} adicionado!</p>`
+        array.innerHTML += `<p>O valor ${valueData} foi adicionado!</p>`
         spaceAnalyze.innerHTML = ""
     }
 }
 
 function analyze() {
-    valores.sort((a,b) => a - b)
-    
-    var register = valores.length
-    var bigger = valores[register -1]
-    var smaller = valores[0]
-    var sum = 0
-    for (let c = 0;c < register; c++) {
-        sum += valores[c]
-    }
-    var average = sum/register
+    if (valores == 0) {
+        alert("[ERRO!] Adicione um número para analisar!")
+    } else {
+        valores.sort((a,b) => a - b) //Assim deixamos a nossa array em ordem crescente
+        
+        let register = valores.length
+        let bigger = valores[register - 1]
+        let smaller = valores[0]
+        let sum = 0
+        // Este for server para fazer a soma dos valores da nossa array
+        for(let c = 0; c < valores.length; c++) { 
+            sum += valores[c]
+        }
+        let average = sum / register
 
-    spaceAnalyze.innerHTML = ""
-    spaceAnalyze.innerHTML += `<p>Ao todo, temos ${register} números cadastrados.</p><br>`
-    spaceAnalyze.innerHTML += `<p>O maior valor informado foi: ${bigger}</p><br>`
-    spaceAnalyze.innerHTML += `<p>O menor valor informado foi: ${smaller}</p><br>`
-    spaceAnalyze.innerHTML += `<p>Somando todos os valores, temos: ${sum}</p><br>`
-    spaceAnalyze.innerHTML += `<p>A média dos valores dgitados é: ${average.toFixed(2)}</p><br>`
+        spaceAnalyze.innerHTML = ""
+        spaceAnalyze.innerHTML += `<p>Ao todo, temos ${register} números cadastrados.</p><br>`
+        spaceAnalyze.innerHTML += `<p>O maior valor informado foi: ${bigger}</p><br>`
+        spaceAnalyze.innerHTML += `<p>O menor valor informado foi: ${smaller}</p><br>`
+        spaceAnalyze.innerHTML += `<p>Somando todos os valores, temos: ${sum}</p><br>`
+        spaceAnalyze.innerHTML += `<p>A média dos valores dgitados é: ${average.toFixed(2)}</p><br>`
+}
 }
